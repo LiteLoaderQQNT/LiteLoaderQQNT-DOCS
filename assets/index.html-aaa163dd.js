768de2401e00c18a41bb676a76f92b6e00ff277d
Night-stars-1 <Night-stars-1@users.noreply.github.com>
2023-08-05 13:14:28 +0000

    Auto deploy from Github Actions

---
+++ assets/index.html-aaa163dd.js
@@ -0,0 +1,20 @@
+import{_ as r,r as s,o as l,c as o,b as d,d as e,e as n,f as a,a as t}from"./app-b5ca2a48.js";const c={},h=e("h1",{id:"liteloaderqqnt",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#liteloaderqqnt","aria-hidden":"true"},"#"),n(" LiteLoaderQQNT")],-1),u=e("p",null,[n("LiteLoaderQQNT是一个QQNT的插件加载器"),e("br"),n(" 它可以让你自由地为QQNT添加各种插件"),e("br"),n(" 比如：美化主题，增加功能，做任何事情 等...")],-1),m={href:"https://t.me/LiteLoaderQQNT",target:"_blank",rel:"noopener noreferrer"},p=t('<h2 id="注意事项" tabindex="-1"><a class="header-anchor" href="#注意事项" aria-hidden="true">#</a> 注意事项</h2><div class="custom-container tip"><p class="custom-container-title">注意</p><ul><li>本项目仍在开发当中，可能会存在一些问题和不足</li><li>仅为个人兴趣而制作，开发目的在于学习和探索</li><li>能力有限，随缘更新。不过也欢迎各位来提交PR</li><li>由于项目特殊性，必要时会停止开发或删除仓库</li></ul></div><h2 id="安装方法" tabindex="-1"><a class="header-anchor" href="#安装方法" aria-hidden="true">#</a> 安装方法</h2><p><a href="install">安装教程</a></p><h2 id="插件开发" tabindex="-1"><a class="header-anchor" href="#插件开发" aria-hidden="true">#</a> 插件开发</h2>',5),_=e("br",null,null,-1),b=e("br",null,null,-1),v={href:"https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template",target:"_blank",rel:"noopener noreferrer"},L=e("br",null,null,-1),Q={href:"https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-List",target:"_blank",rel:"noopener noreferrer"},g=t(`<blockquote><p>Windows QQNT 9.8.5版本及以上暂时无法打开自身的DevTools<br> 请安装Chii Devtools插件（推荐）或QQNT vConsole插件进行调试</p></blockquote><h2 id="数据目录" tabindex="-1"><a class="header-anchor" href="#数据目录" aria-hidden="true">#</a> 数据目录</h2><p>LiteLoaderQQNT的默认数据文件夹在<code>用户目录/Documents/LiteLoaderQQNT</code><br> 修改环境变量<code>LITELOADERQQNT_PROFILE</code>可指定目录位置</p><p>数据目录结构：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>LiteLoaderQQNT
+    ├─plugins           // 插件本体目录
+    │   ├─my-plugin         // 插件本体
+    │   └─...
+    ├─plugins_cache     // 插件缓存目录
+    │   ├─my-plugin
+    │   └─...
+    ├─plugins_data      // 插件数据目录
+    │   ├─my-plugin
+    │   └─...
+    └─config.json       // LiteLoader配置文件
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="开源协议" tabindex="-1"><a class="header-anchor" href="#开源协议" aria-hidden="true">#</a> 开源协议</h2><p><a href="./LICENSE">MIT License</a><br> Copyright (c) 2023 沫烬染</p>`,7);function N(f,T){const i=s("ExternalLinkIcon");return l(),o("div",null,[d(`
+ * @Author: Night-stars-1 nujj1042633805@gmail.com
+ * @Date: 2023-05-23 15:45:31
+ * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
+ * @LastEditTime: 2023-08-04 22:14:36
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
+`),h,u,e("p",null,[n("Telegram闲聊群："),e("a",m,[n("https://t.me/LiteLoaderQQNT"),a(i)])]),p,e("p",null,[n("如需上架插件市场，要使用最新的manifest格式（建议参考已上架插件，文件来源可使用仓库源码，也可用Release指定文件）"),_,n(" 并在Github开源，需要打包的插件请在仓库发布Release，文件尽量使用Github Actions打包"),b,n(" 插件模板："),e("a",v,[n("LiteLoaderQQNT-Plugin-Template"),a(i)]),L,n(" 插件仓库："),e("a",Q,[n("LiteLoaderQQNT-Plugin-List"),a(i)])]),g])}const E=r(c,[["render",N],["__file","index.html.vue"]]);export{E as default};
